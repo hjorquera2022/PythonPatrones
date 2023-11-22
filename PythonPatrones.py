e2e2c4ed0238f4836b7478ff83b1f5dc95e1ee0a
hjorquera2022 <hjorquera@icsa-metaproject.cl>
2023-11-22 11:36:22 -0300

    Agregar archivos de proyecto.

---
+++ PythonPatrones.py
@@ -0,0 +1,38 @@
+﻿# Dibuja un tablero de ajedrez de 8x8 con un peón
+
+# Define el tamaño del tablero
+filas = 8
+columnas = 8
+
+# Define la representación del peón
+peon = "♟"
+
+# Define la representación vacía de una casilla
+casilla_vacia = "."
+
+# Define el patrón del dibujo del peón
+patron_peon = [
+    [" ", " ", " ", ".", ".", "."  " ", " "],
+    [" ", " ", " ", ".", ".", " ", " ", " "],
+    [".", ".", ".", ".", ".", ".", ".", "."],
+    [".", ".", ".", ".", ".", ".", ".", "."],
+    [".", ".", ".", ".", ".", ".", ".", "."],
+    [".", ".", ".", ".", ".", ".", ".", "."],
+    [".", ".", ".", ".", ".", ".", ".", "."],
+    [".", ".", ".", ".", ".", ".", ".", "."]
+]
+
+# Dibuja el tablero con el dibujo del peón
+for fila in range(filas):
+    for columna in range(columnas):
+        if (fila + columna) % 2 == 0:
+            # Casilla blanca
+            if patron_peon[fila][columna] == ".":
+                print(" ", end="")
+            else:
+                print(peon, end="")
+        else:
+            # Casilla negra
+            print("#", end="")
+    
+    print()  # Salto de línea entre filas
